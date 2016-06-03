55474c902d0c9af8650e37b509602a518192d91e
Ruben de Laat <ruben@logic-labs.nl>
2016-06-03 15:07:02 +0200

    Added OuterArea demo service

--- DemoPlugins/src/org/bimserver/demoplugins/outerarea/OuterAreaServicePlugin.java
+++ DemoPlugins/src/org/bimserver/demoplugins/outerarea/OuterAreaServicePlugin.java
@@ -1,29 +1,27 @@
 package org.bimserver.demoplugins.outerarea;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Random;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.bimserver.emf.IfcModelInterface;
+import org.bimserver.geometry.Vector;
 import org.bimserver.interfaces.objects.SObjectType;
 import org.bimserver.interfaces.objects.SProject;
 import org.bimserver.models.geometry.GeometryData;
 import org.bimserver.models.geometry.GeometryInfo;
-import org.bimserver.models.ifc2x3tc1.IfcBoolean;
-import org.bimserver.models.ifc2x3tc1.IfcObject;
 import org.bimserver.models.ifc2x3tc1.IfcProduct;
-import org.bimserver.models.ifc2x3tc1.IfcProperty;
-import org.bimserver.models.ifc2x3tc1.IfcPropertySet;
-import org.bimserver.models.ifc2x3tc1.IfcPropertySetDefinition;
-import org.bimserver.models.ifc2x3tc1.IfcPropertySingleValue;
-import org.bimserver.models.ifc2x3tc1.IfcRailing;
-import org.bimserver.models.ifc2x3tc1.IfcRelDefines;
-import org.bimserver.models.ifc2x3tc1.IfcRelDefinesByProperties;
+import org.bimserver.models.ifc2x3tc1.IfcSpace;
+import org.bimserver.models.ifc2x3tc1.IfcWallStandardCase;
 import org.bimserver.models.ifc2x3tc1.Tristate;
 import org.bimserver.plugins.services.AbstractAddExtendedDataService;
 import org.bimserver.plugins.services.BimServerClientInterface;
-import org.bimserver.plugins.services.Geometry;
+import org.bimserver.utils.IfcUtils;
 
 import com.google.common.base.Charsets;
 
@@ -33,97 +31,94 @@ public class OuterAreaServicePlugin extends AbstractAddExtendedDataService {
 		super("outerareanamespace");
 	}
 
-	public Tristate getProperty(IfcObject ifcObject) {
-		for (IfcRelDefines ifcRelDefines : ifcObject.getIsDefinedBy()) {
-			if (ifcRelDefines instanceof IfcRelDefinesByProperties) {
-				IfcRelDefinesByProperties ifcRelDefinesByProperties = (IfcRelDefinesByProperties)ifcRelDefines;
-				IfcPropertySetDefinition propertySetDefinition = ifcRelDefinesByProperties.getRelatingPropertyDefinition();
-				if (propertySetDefinition instanceof IfcPropertySet) {
-					IfcPropertySet ifcPropertySet = (IfcPropertySet)propertySetDefinition;
-					for (IfcProperty ifcProperty : ifcPropertySet.getHasProperties()) {
-						if (ifcProperty instanceof IfcPropertySingleValue) {
-							IfcPropertySingleValue propertyValue = (IfcPropertySingleValue)ifcProperty;
-							if (ifcProperty.getName().equals("IsExternal")) {
-								IfcBoolean label = (IfcBoolean)propertyValue.getNominalValue();
-								return label.getWrappedValue();
-							}
-						}
-					}
-				}
-			}
-		}
-		return null;
-	}
-	
 	@Override
 	public void newRevision(RunningService runningService, BimServerClientInterface bimServerClientInterface, long poid,
 			long roid, String userToken, long soid, SObjectType settings) throws Exception {
 		
 		SProject project = bimServerClientInterface.getServiceInterface().getProjectByPoid(poid);
-		IfcModelInterface model = bimServerClientInterface.getModel(project, roid, true, false);
+		IfcModelInterface model = bimServerClientInterface.getModel(project, roid, true, false, true);
 		
 		int nrTriangles = 0;
 		int nrVertices = 0;
 		int nrUsedProducts = 0;
+		float totalArea = 0f;
+		
+		Map<String, AtomicInteger> map = new HashMap<>();
+
+		float totalSpaceM3 = 0;
+		
+		for (IfcSpace ifcSpace : model.getAll(IfcSpace.class)) {
+			Double volume = IfcUtils.getIfcQuantityVolume(ifcSpace, "Net Volume");
+			if (volume != null) {
+				totalSpaceM3 += volume;
+			}
+		}
 		
 		List<IfcProduct> allWithSubTypes = model.getAllWithSubTypes(IfcProduct.class);
-		int i=0;
 		for (IfcProduct ifcProduct : allWithSubTypes) {
-//			System.out.println((100 * i / allWithSubTypes.size()) + "%");
-			i++;
-			if (ifcProduct instanceof IfcRailing) {
+			if (!map.containsKey(ifcProduct.eClass().getName())) {
+				map.put(ifcProduct.eClass().getName(), new AtomicInteger(0));
+			}
+			if (!(ifcProduct instanceof IfcWallStandardCase)) {
 				continue;
 			}
 			
-			Tristate isExternal = getProperty(ifcProduct);
+			Tristate isExternal = IfcUtils.getBooleanProperty(ifcProduct);
 			if (isExternal == Tristate.TRUE) {
 				GeometryInfo geometryInfo = ifcProduct.getGeometry();
 				if (geometryInfo != null) {
-					nrTriangles += geometryInfo.getPrimitiveCount();
-					GeometryData data = geometryInfo.getData();
-					if (data != null) {
-						nrVertices += data.getVertices().length / 12;
+					GeometryData geometryData = geometryInfo.getData();
+					if (geometryData != null) {
+						map.get(ifcProduct.eClass().getName()).incrementAndGet();
+						nrUsedProducts++;
+						
+						ByteBuffer indicesBuffer = ByteBuffer.wrap(geometryData.getIndices());
+						indicesBuffer.order(ByteOrder.LITTLE_ENDIAN);
+						IntBuffer indices = indicesBuffer.asIntBuffer();
+
+						ByteBuffer verticesBuffer = ByteBuffer.wrap(geometryData.getVertices());
+						verticesBuffer.order(ByteOrder.LITTLE_ENDIAN);
+						FloatBuffer vertices = verticesBuffer.asFloatBuffer();
+
+						nrTriangles += (indices.capacity() / 3);
+						nrVertices += (vertices.capacity() / 3);
+
+						float objectAream2 = 0;
+						
+						for (int i=0; i<indices.capacity(); i+=3) {
+							float[][] triangle = new float[3][3];
+							for (int x=0; x<3; x++) {
+								triangle[x] = new float[]{
+									vertices.get(indices.get(i + x) * 3),
+									vertices.get(indices.get(i + x) * 3 + 1),
+									vertices.get(indices.get(i + x) * 3 + 2)
+								};
+							}
+							
+							// Dividing by 2 is a very rough approximation, ignoring the width of objects
+							float areaTriangle = Vector.getArea(triangle) / 2;
+							objectAream2 += areaTriangle / 1000000f;
+						}
+						totalArea += objectAream2;
 					}
-					nrUsedProducts++;
 				}
-
-				Geometry geometry = bimServerClientInterface.getGeometry(roid, ifcProduct);
-				
-				if (geometry == null) {
-					continue;
-				}
-
-
-				IntBuffer indices = geometry.getIndices();
-				FloatBuffer vertices = geometry.getVertices();
-				
-				nrTriangles += (indices.capacity() / 3);
-				nrVertices += (vertices.capacity() / 3);
-				
-//				for (int i=0; i<indices.capacity(); i+=3) {
-//					float[][] triangle = new float[3][3];
-//					for (int x=0; x<3; x++) {
-//						for (int y=0; y<3; y++) {
-//							triangle[x] = new float[]{
-//								vertices.get(indices.get(i + x) * 3),
-//								vertices.get(indices.get(i + x) * 3 + 1),
-//								vertices.get(indices.get(i + x) * 3 + 2)
-//							};
-//						}
-//					}
-//				}
 			}
-			
 		}
-		float area = new Random().nextFloat();
 		
 		StringBuilder results = new StringBuilder();
 		results.append("All IfcProduct subtypes (excluding IfcRailing) which are external\n");
-		results.append("Nr products: " + nrUsedProducts + "\n");
+		for (String type : map.keySet()) {
+			int count = map.get(type).get();
+			if (count > 0) {
+				results.append("\t" + type + ": " + count + "\n");
+			}
+		}
+		results.append("Total nr products: " + nrUsedProducts + "\n");
 		results.append("Nr triangles: " + nrTriangles + "\n");
 		results.append("Nr vertices: " + nrVertices + "\n");
-		results.append("Total area (rough estimation / random): " + area + "\n");
+		results.append("Total IfcSpace Net Volumes: " + totalSpaceM3 + "m3\n");
+		results.append("Total area (rough estimation): " + totalArea + " m2\n");
 		
-		addExtendedData(results.toString().getBytes(Charsets.UTF_8), "test.txt", "Test", "text", bimServerClientInterface, roid);
+		addExtendedData(results.toString().getBytes(Charsets.UTF_8), "test.txt", "Test", "text/plain", bimServerClientInterface, roid);
 	}
 }
