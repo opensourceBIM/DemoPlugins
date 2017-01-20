f4daf9603a42765d4c381327d47c0d24caaa4f8c
Ruben de Laat <ruben@logic-labs.nl>
2017-01-20 09:15:38 +0100

    Warnings

--- DemoPlugins/src/org/bimserver/demoplugins/pathchecker/RunStandalone.java
+++ DemoPlugins/src/org/bimserver/demoplugins/pathchecker/RunStandalone.java
@@ -1,18 +1,5 @@
 package org.bimserver.demoplugins.pathchecker;
 
-import java.util.List;
-
-import org.apache.log4j.chainsaw.Main;
-import org.bimserver.interfaces.objects.SProject;
-import org.bimserver.models.ifc2x3tc1.IfcClassificationNotationSelect;
-import org.bimserver.models.ifc2x3tc1.IfcClassificationReference;
-import org.bimserver.models.ifc2x3tc1.IfcSpace;
-import org.bimserver.shared.ChannelConnectionException;
-import org.bimserver.shared.UsernamePasswordAuthenticationInfo;
-import org.bimserver.shared.exceptions.BimServerClientException;
-import org.bimserver.shared.exceptions.ServiceException;
-import org.bimserver.utils.IfcUtils;
-
 public class RunStandalone {
 	public static void main(String[] args) {
 //		new RunStandalone().start();
