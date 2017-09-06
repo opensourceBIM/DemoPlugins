cf27b5883eecef6cd7e79e4175060841ed3aa60a
Ruben de Laat <ruben@logic-labs.nl>
2017-09-06 11:50:39 +0200

    Updated

--- DemoPlugins/src/org/bimserver/demoplugins/bimbotdemo/BimBotDemoService.java
+++ DemoPlugins/src/org/bimserver/demoplugins/bimbotdemo/BimBotDemoService.java
@@ -3,20 +3,16 @@ package org.bimserver.demoplugins.bimbotdemo;
 import org.bimserver.bimbots.BimBotsException;
 import org.bimserver.bimbots.BimBotsInput;
 import org.bimserver.bimbots.BimBotsOutput;
-import org.bimserver.bimbots.BimBotsServiceInterface;
 import org.bimserver.emf.IfcModelInterface;
-import org.bimserver.interfaces.objects.SObjectType;
 import org.bimserver.models.geometry.GeometryInfo;
 import org.bimserver.models.ifc2x3tc1.IfcProduct;
 import org.bimserver.models.store.ObjectType;
-import org.bimserver.models.store.ServiceDescriptor;
 import org.bimserver.plugins.SchemaName;
-import org.bimserver.plugins.services.AbstractService;
-import org.bimserver.plugins.services.BimServerClientInterface;
+import org.bimserver.plugins.services.BimBotAbstractService;
 
 import com.google.common.base.Charsets;
 
-public class BimBotDemoService extends AbstractService implements BimBotsServiceInterface {
+public class BimBotDemoService extends BimBotAbstractService {
 
 	@Override
 	public BimBotsOutput runBimBot(BimBotsInput input, ObjectType settings) throws BimBotsException {
@@ -38,13 +34,5 @@ public class BimBotDemoService extends AbstractService implements BimBotsService
 		output.setTitle("BimBotDemoService Results");
 		output.setContentType("text/plain");
 		return output;
-	}
-
-	@Override
-	public void newRevision(RunningService runningService, BimServerClientInterface bimServerClientInterface, long poid, long roid, String userToken, long soid, SObjectType settings) throws Exception {
-	}
-
-	@Override
-	public void addRequiredRights(ServiceDescriptor serviceDescriptor) {
 	}
 }
